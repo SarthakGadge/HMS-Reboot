663c12bcdc84312be447b1de6e0c76dfbf33372d
SarthakGadge <sarthakgadge43@gmail.com>
2024-10-24 15:07:17 +0530

    inventory api change

--- hmsbackend/inventory/models.py
+++ hmsbackend/inventory/models.py
@@ -17,7 +17,8 @@ class Inventory(models.Model):
         max_length=20, choices=STATUS_CHOICES, default='not_approved')
     admin = models.ForeignKey(
         Admin, on_delete=models.SET_NULL, null=True, blank=True)
-    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    staff = models.ForeignKey(
+        Staff, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"Inventory(description={self.description}, status={self.status})"
--- hmsbackend/inventory/views.py
+++ hmsbackend/inventory/views.py
@@ -10,25 +10,47 @@ from userauth.models import Staff, Admin
 
 class InventoryCreateView(APIView):
 
+    def get(self, request):
+        if request.user.role not in ['admin', 'superadmin', 'staff']:
+            return Response({"msg": "You do not have the permission to perform this action"}, status=status.HTTP_401_UNAUTHORIZED)
+        inventory = Inventory.objects.all()
+        serializer = InventorySerializer(inventory, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request, *args, **kwargs):
 
         user = request.user.id
 
-        if request.user.role not in ['admin', 'staff', 'superadmin']:
+        if request.user.role not in ['staff', 'admin']:
             return Response({"msg": "You do not have the authorization for this action"}, status=status.HTTP_401_UNAUTHORIZED)
 
-        staff = get_object_or_404(Staff, user_id=user)
-        data = request.data.copy()
-        data['status'] = 'not_approved'
-        data['staff'] = staff.id
-        data['admin'] = None
-        serializer = InventorySerializer(data=data)
+        if request.user.role == 'staff':
+            staff = get_object_or_404(Staff, user_id=user)
+            data = request.data.copy()
+            data['status'] = 'not_approved'
+            data['staff'] = staff.id
+            data['admin'] = None
+            serializer = InventorySerializer(data=data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        elif request.user.role == 'admin':
+            admin = get_object_or_404(Admin, user_id=user)
+            data = request.data.copy()
+            data['status'] = 'approved'
+            data['admin'] = admin.id
+            data['staff'] = None
+            serializer = InventorySerializer(data=data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, pk, *args, **kwargs):
 
